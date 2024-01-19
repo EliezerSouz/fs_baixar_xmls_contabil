86321ce916802e0337f7ab6efffa6d728182b1dc
Arlei F. Farnetani Junior <farneetani@gmail.com>
2024-01-19 17:37:05 -0300

    Update 19012024 1737

--- lib/config/configuracoes_screen.dart
+++ lib/config/configuracoes_screen.dart
@@ -44,8 +44,8 @@ class _ConfiguracoesScreenState extends State<ConfiguracoesScreen> {
             _buildTextField(_hostController, 'Host', 'localhost'),
             _buildTextField(_portController, 'Porta', '3306'),
             _buildTextField(_userController, 'Usuário', 'root'),
-            _buildTextField(_passController, 'Senha', '', isPassword: true),
-            _buildTextField(_nameController, 'Nome do Banco de Dados', ''),
+            _buildTextField(_passController, 'Senha', 'passwor', isPassword: true),
+            _buildTextField(_nameController, 'Nome do Banco de Dados', 'db_name'),
             const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () async {
